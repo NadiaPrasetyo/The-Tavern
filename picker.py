06d7f9f7aae0afc10d66a788decca73c8387ab6d
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-09-11 23:16:49 +1200

    Recipe tab

---
+++ picker.py
@@ -0,0 +1,40 @@
+
+from pymongo import MongoClient
+
+import winsound
+
+from dotenv import load_dotenv
+import os
+
+
+# connect to MongoDB from the .env file MONGODB_URI
+load_dotenv()
+
+MONGODB_URI = os.getenv('MONGODB_URI')
+client = MongoClient(MONGODB_URI)
+
+collection = client['The-tavern']['RecipeList']
+
+tags = ["Dessert", "Wok Of Life"]
+
+# get recipes that has the tag from "Tag" field which is an array
+recipes = collection.find({"Tag": {"$all": tags}})
+
+print()
+# print the recipes
+for recipe in recipes:
+    print(recipe['Name'] + "\n")
+    print(recipe['Link'])
+    print(recipe['Ingredients'])
+    print(recipe['Tag'])
+    
+    print()
+    
+    winsound.PlaySound("public/violin-spiccato-g2-91380.wav", winsound.SND_FILENAME)
+    # Ask the user to keep or delete the recipe
+    inp = input("Keep (enter) or delete (\"del\")?")
+    if inp == "del":
+        collection.delete_one({'Name': recipe['Name']})
+        print("Deleted")
+    
+    
--- src/components/RecipeTab.js
+++ src/components/RecipeTab.js
@@ -1,53 +1,115 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { TbSearch } from "react-icons/tb";
+import { IoIosHourglass } from "react-icons/io";
+import { MdOutlineStarPurple500 } from "react-icons/md";
+import { LuFilter } from "react-icons/lu";
+import { LuBookOpenCheck } from "react-icons/lu";
 import '../App.css';
 
 const RecipeTab = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-  
-    // Toggle the book sliding in and out
-    const toggleBook = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    // Change the content of the book based on the clicked bookmark
-    const changePage = (page) => {
-      setCurrentPage(page);
-    };
-  
-    return (
-      <div className={`book-container ${isOpen ? "open" : ""}`}>
-        {/* Book content area */}
-        <div className="book-content">
-          <div className="book-header">
-            <h2>Book Title</h2>
-            <button className="close-btn" onClick={toggleBook}>
-              {isOpen ? "Close" : "Open"}
-            </button>
-          </div>
-          <div className="book-page">
-            {currentPage === 1 && <div>Page 1 Content</div>}
-            {currentPage === 2 && <div>Page 2 Content</div>}
-            {currentPage === 3 && <div>Page 3 Content</div>}
-          </div>
-        </div>
-  
-        {/* Bookmarks */}
-        <div className="bookmarks">
-          <button onClick={() => changePage(1)} className="bookmark bookmark-1">
-            Page 1
-          </button>
-          <button onClick={() => changePage(2)} className="bookmark bookmark-2">
-            Page 2
-          </button>
-          <button onClick={() => changePage(3)} className="bookmark bookmark-3">
-            Page 3
-          </button>
-        </div>
-      </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Toggle the tab when clicking anywhere on the book container except for bookmarks or content
+  const toggleBook = () => {
+    setIsOpen(!isOpen);
   };
 
+  // Change the content of the book based on the clicked bookmark
+  const changePage = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div className={`recipe-tab-container ${isOpen ? "open" : ""}`}>
+      {/* Book content area */}
+      <div className='book-container background' onClick={toggleBook}>
+        <div className="book-container" onClick={toggleBook}>
+          <div className="book-content">
+            {currentPage === 1 && 
+              <div className='browse-recipe'>
+                <div className='tab-title'>BROWSE RECIPES</div>
+                <div className='search-bar-container' onClick={(e) => e.stopPropagation()}>
+                  <input className='search-bar' type='text' placeholder='Search for recipes' />
+                  <TbSearch className='search-icon' />
+                  <LuFilter className='filter-icon' />
+                </div>
+                <div className='recipe-containers'>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 1</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 2</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 3</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 4</div>
+                </div>
+              </div>
+            }
+            {currentPage === 2 && 
+              <div className='recommendation-tab'>
+                <div className='tab-title'>RECOMMENDATION</div>
+                <div className='search-bar-container' onClick={(e) => e.stopPropagation()}>
+                  <input className='search-bar' type='text' placeholder='Search for recipes' />
+                  <TbSearch className='search-icon' />
+                  <LuFilter className='filter-icon' />
+                </div>
+                <div className='recipe-containers'>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 1</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 2</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 3</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 4</div>
+                </div>
+              </div>
+            }
+            {currentPage === 3 && 
+              <div className='history-tab'>
+                <div className='tab-title'>HISTORY</div>
+                <div className='search-bar-container' onClick={(e) => e.stopPropagation()}>
+                  <input className='search-bar' type='text' placeholder='Search for recipes' />
+                  <TbSearch className='search-icon' />
+                  <LuFilter className='filter-icon' />
+                </div>
+                <div className='recipe-containers'>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 1</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 2</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 3</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 4</div>
+                </div>
+              </div>
+            }
+            {currentPage === 4 &&
+              <div className='favourite-tab'>
+                <div className='tab-title'>FAVOURITES</div>
+                <div className='search-bar-container' onClick={(e) => e.stopPropagation()}>
+                  <input className='search-bar' type='text' placeholder='Search for recipes' />
+                  <TbSearch className='search-icon' />
+                  <LuFilter className='filter-icon' />
+                </div>
+                <div className='recipe-containers'>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 1</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 2</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 3</div>
+                  <div className='recipe-list' onClick={(e) => e.stopPropagation()}>Recipe 4</div>
+                </div>
+              </div>
+            }
+            </div>
+        </div>
+      </div>
+      {/* Bookmarks (always visible) */}
+      <div className={`bookmarks ${isOpen ? "show" : ""}`} onClick={(e) => e.stopPropagation()}>
+        <div onClick={() => changePage(1)} className="bookmark bookmark-1">
+          <TbSearch size={30} />
+        </div>
+        <div onClick={() => changePage(2)} className="bookmark bookmark-2">
+          <LuBookOpenCheck size={30}/>
+        </div>
+        <div onClick={() => changePage(3)} className="bookmark bookmark-3">
+          <IoIosHourglass size={30}/>
+        </div>
+        <div onClick={() => changePage(4)} className="bookmark bookmark-4">
+          <MdOutlineStarPurple500 size={30}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default RecipeTab;
-
-
