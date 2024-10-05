ef74395a383602f8665b029e9eafd65f88cec84c
Nadia Prasetyo <prana001@student.otago.ac.nz>
2024-10-05 16:59:28 +1300

    adding landing page styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,3 +1,4 @@
+import { on } from 'nodemailer/lib/xoauth2';
 import '../App.css';
 import React, { useState } from 'react';
 
@@ -9,6 +10,8 @@ function Landing() {
   sessionStorage.clear();
   localStorage.setItem('isDarkMode', isDarkMode);
 
+  const [onSection, setOnSection] = useState("section1");
+
   return (
     
     <div className="landingPage">
@@ -16,20 +19,24 @@ function Landing() {
             <img src = "Tavern-logo-small.png" alt = "Tavern Logo"/>
             <a className="loginOrRegisterButton" href = "/Login"> Login/Register </a>
         </header>
-        <div className="section1">
-            <img id="croissant" src="food-pics/16.jpg" alt="croissant" />
-            <img id="egg" src="food-pics/17.jpg" alt="egg" />
-            <img id="carrot" src="food-pics/18.jpg" alt="carrot" />
-            <img id="soup" src="food-pics/19.jpg" alt="soup" />
-            <img id="noodle" src="food-pics/20.jpg" alt="noodle" />
-            <img id="cow" src="food-pics/21.jpg" alt="cow" />
-            <img id="burger" src="food-pics/22.jpg" alt="burger" />
-            <img id="beer" src="food-pics/23.jpg" alt="beer" />
-            <img id="tomato" src="food-pics/24.jpg" alt="tomato" />
-            <img id="ricebowl" src="food-pics/25.jpg" alt="ricebowl" />
-            <img id="cake" src="food-pics/26.jpg" alt="cake" />
-            <img id="spinach" src="food-pics/27.jpg" alt="spinach" />
-            <img id="donut" src="food-pics/28.jpg" alt="donut" />
+
+        <span className="artifactContainer">
+            <img className= {onSection} id="croissant" src="food-pics/16.jpg" alt="croissant" />
+            <img className= {onSection} id="egg" src="food-pics/17.jpg" alt="egg" />
+            <img className= {onSection} id="carrot" src="food-pics/18.jpg" alt="carrot" />
+            <img className= {onSection} id="soup" src="food-pics/19.jpg" alt="soup" />
+            <img className= {onSection} id="noodle" src="food-pics/20.jpg" alt="noodle" />
+            <img className= {onSection} id="cow" src="food-pics/21.jpg" alt="cow" />
+            <img className= {onSection} id="burger" src="food-pics/22.jpg" alt="burger" />
+            <img className= {onSection} id="beer" src="food-pics/23.jpg" alt="beer" />
+            <img className= {onSection} id="tomato" src="food-pics/24.jpg" alt="tomato" />
+            <img className= {onSection} id="ricebowl" src="food-pics/25.jpg" alt="ricebowl" />
+            <img className= {onSection} id="cake" src="food-pics/26.jpg" alt="cake" />
+            <img className= {onSection} id="spinach" src="food-pics/27.jpg" alt="spinach" />
+            <img className= {onSection} id="donut" src="food-pics/28.jpg" alt="donut" />
+        </span>
+
+        <div className="section1">   
         </div>
         <div className="section2">
         </div>
