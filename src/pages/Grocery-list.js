7b31aba0ad0b2a67f859b9a0a5095ec18446eb44
Nadia Prasetyo <prana001@student.otago.ac.nz>
2024-10-08 23:08:25 +1300

    Adding comments to grocery list

--- src/pages/Grocery-list.js
+++ src/pages/Grocery-list.js
@@ -5,18 +5,27 @@ import ProfileBar from '../components/profilebar.js';
 import { IoAddCircle } from "react-icons/io5";
 import { AiOutlineEdit } from "react-icons/ai";
 
+/**
+ * Grocery list component
+ * @param {Object} userdata - user data
+ * @returns the Grocery component
+ */
 function Grocery({userdata}) {
-  const [groceryItemOpen, setGroceryItemOpen] = useState({});
-  const [groceryItemValue, setGroceryItemValue] = useState('');
-  const [groceryList, setGroceryList] = useState([]);
-  const [inputValues, setInputValues] = useState({});
+  const [groceryItemOpen, setGroceryItemOpen] = useState({});//state to track which grocery item is open
+  const [groceryItemValue, setGroceryItemValue] = useState('');//state to track the value of the grocery item
+  const [groceryList, setGroceryList] = useState([]);//state to track the grocery list
+  const [inputValues, setInputValues] = useState({});//state to track the input values
   const [isEditable, setIsEditable] = useState({}); // State to track which items are editable
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);//state to track if the page is loading
 
   const isTouchDevice = window.matchMedia('(pointer: coarse)').matches;
 
+  /**
+   * Function to get the grocery list
+   * @param {Event} e - the event
+   */
   const getGrocery = async (e) => {
-    setIsLoading(true);
+    setIsLoading(true);//set loading to true while fetching data
     try {
       const response = await fetch('/api/get-grocery', {
         method: 'POST',
@@ -31,28 +40,29 @@ function Grocery({userdata}) {
       const grocery = await response.json();
 
       if (response.status === 200) {
-        setGroceryList(grocery.grocery);
+        setGroceryList(grocery.grocery);//set the grocery list: update the state
       } else {
         console.log("Error getting grocery");
       }
     } catch (error) {
       console.error('Error:', error);
     }
-    setIsLoading(false);
+    setIsLoading(false);//set loading to false after fetching data
   }
 
+  // Function to get the grocery list on page load
   useEffect(() => {
     getGrocery();
   }, []);
 
-
+  //prevent the submission of the form when the enter key is pressed
   document.onkeydown = function (event) {
     if (event.keyCode === 13) {
       event.preventDefault();
       const addGroceryButtons = document.querySelectorAll('.addGrocery');
       for (let i = 0; i < addGroceryButtons.length; i++) {
-        if (addGroceryButtons[i].style.left === '250px') {
-          addGroceryButtons[i].click();
+        if (addGroceryButtons[i].classList.contains('move-right')) {//if the button is open
+          addGroceryButtons[i].click();//click the button
           break;
         }
       }
@@ -66,6 +76,7 @@ function Grocery({userdata}) {
     categoryList[category] = groceryList.filter(item => item.Category === category);
   });
 
+  // Update the input values and isEditable state when the grocery list changes
   useEffect(() => {
     if (groceryList.length > 0) {
       const categories = [...new Set(groceryList.map(item => item.Category))];
@@ -80,12 +91,16 @@ function Grocery({userdata}) {
       setIsEditable(newIsEditable);
       setInputValues(newInputValues);
     }
-  }, [groceryList]);
+  }, [groceryList]);//on grocery list change
 
+  /**
+   * Function to add a grocery item
+   * @param {String} item - the grocery item
+   * @param {String} category - the category of the grocery item
+   */
   function addGroceryItem(item, category) {
-    console.log("Adding grocery item: " + item + " to category: " + category);
     // Add the grocery item to the database
-
+    // If the item already exists in the grocery list, update the name
     fetch('/api/add-grocery-item', {
       method: 'POST',
       headers: {
@@ -123,8 +138,12 @@ function Grocery({userdata}) {
     });
   }
 
+  /**
+   * Function to add a grocery item
+   * @param {String} category - the category of the grocery item
+   */
   function addGrocery(category) {
-    if (groceryItemOpen[category] === undefined) {
+    if (groceryItemOpen[category] === undefined) {//if the category is not in the groceryItemOpen state, it initializes it
       setGroceryItemOpen({
         ...groceryItemOpen,
         [category]: false
@@ -132,7 +151,7 @@ function Grocery({userdata}) {
     }
     const isOpen = groceryItemOpen[category];
 
-    if (!isOpen) {
+    if (!isOpen) {//open the input field if it is closed
       setGroceryItemOpen({
         ...groceryItemOpen,
         [category]: true
@@ -140,7 +159,7 @@ function Grocery({userdata}) {
     } else {
       const value = groceryItemValue.trim();
       if (value !== '') {
-        addGroceryItem(value, category);
+        addGroceryItem(value, category);//add the grocery item
         setGroceryItemValue('');
       }
       setGroceryItemOpen({
@@ -150,25 +169,30 @@ function Grocery({userdata}) {
     }
   }
 
+  /**
+   * Function to add a grocery category
+   * @param {Event} event - the event
+   * @param {Boolean} confirmedPopup - the boolean to check if the popup is confirmed
+   */
   function addCategory(event, confirmedPopup) {
-    const buttonClick = event.target;
+    const buttonClick = event.target;//get the button that was clicked
 
     if (!confirmedPopup) {
       //get the source button
       const popup = document.querySelector('.popupAddCategory');
-      //check if popup is already open
+      //check if popup is already open and close it if it is
       if (popup.style.display === 'block') {
         popup.style.display = 'none';
         return;
       }
 
-      popup.style.display = 'block';
+      popup.style.display = 'block';//display the popup
 
     } else {
-      const categoryElement = buttonClick.closest('.popupAddCategory').querySelector('.addCategoryItem');
+      const categoryElement = buttonClick.closest('.popupAddCategory').querySelector('.addCategoryItem');//get the input field
       const category = categoryElement.value.trim();
 
-      if (category === '') {
+      if (category === '') {//if the category is empty, close the popup and return
         //close the popup
         document.querySelector('.popupAddCategory').style.display = 'none';
         //clear the popup input field
@@ -176,6 +200,7 @@ function Grocery({userdata}) {
         return;
       }
 
+      // Add the category to the database
       fetch('/api/add-grocery-category', {
         method: 'POST',
         headers: {
@@ -187,24 +212,31 @@ function Grocery({userdata}) {
           Name: ''
         }),
       }).then((response) => {
-        if (response.status === 200) {
+        if (response.status === 200) {//if the category is added successfully, update the grocery list
           console.log("Category added successfully");
           setGroceryItemOpen({
             ...groceryItemOpen,
             [category]: false
           });
 
-          setGroceryList([...groceryList, 
+          setGroceryList([...groceryList, //update the grocery list
             { Name: '', Category: category }]);
         } else {
           console.log("Error adding category");
         }
       });
-      categoryElement.value = '';
-      document.querySelector('.popupAddCategory').style.display = 'none';
+      categoryElement.value = '';//clear the input field
+      document.querySelector('.popupAddCategory').style.display = 'none';//close the popup
     }
   }
 
+  /**
+   * Function to handle input change for edit item
+   * @param {String} category - the category of the grocery item
+   * @param {Event} e - the event
+   * @param {Number} index - the index of the grocery item
+   * @param {String} field - the field of the grocery item
+   */
   const handleInputChange = (category, e, index, field) => {
     const { value } = e.target;
     setInputValues(prevValues => ({
@@ -213,6 +245,11 @@ function Grocery({userdata}) {
     }));
   };
 
+  /**
+   * Function to handle edit click for grocery item
+   * @param {String} category - the category of the grocery item
+   * @param {Number} index - the index of the grocery item
+   */
   const handleEditClick = (category, index) => {
 
     if (isEditable[category][index]) {
@@ -225,20 +262,16 @@ function Grocery({userdata}) {
           ...isEditable,
           [category]: isEditable[category].map((item, i) => (i === index ? !item : item))
         });
-        return;
+        return;//if the item is not changed, return
       }
-
-      console.log("Original Name:", originalName);
-
-      console.log("Updating item:", item);
-
+      // Update the grocery item in the database with the new name and category
       fetch('/api/update-grocery-item', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          Username: userdata.username,   // Username
+          Username: userdata.username,                  // Username
           Name: originalName,                           // Original Name (to find the document)
           Category: originalCategory,                   // Original Category (optional for matching)
           NewName: item.Name,                           // New Name after editing
@@ -246,7 +279,7 @@ function Grocery({userdata}) {
         }),
       }).then(response => {
         if (response.status === 200) {
-          setGroceryList(
+          setGroceryList(//update the grocery list
             groceryList.map((element) => {
               if (element.Name === originalName) {
                 return { Name: item.Name, Category: item.Category };
@@ -261,11 +294,16 @@ function Grocery({userdata}) {
     }
     setIsEditable({
       ...isEditable,
-      [category]: isEditable[category].map((item, i) => (i === index ? !item : item))
+      [category]: isEditable[category].map((item, i) => (i === index ? !item : item))//toggle the edit mode
     });
 
   };
 
+  /**
+   * Function to remove a grocery item
+   * @param {String} category - the category of the grocery item
+   * @param {String} itemName - the name of the grocery item
+   */
   const removeItem = (category, itemName) => {
     console.log("Removing item: " + itemName);
     fetch('/api/remove-grocery-item', {
@@ -281,20 +319,26 @@ function Grocery({userdata}) {
     }).then((response) => {
       if (response.status === 200) {
         console.log("Item removed successfully");
-        setGroceryList(groceryList.filter(item => item.Name !== itemName));
+        setGroceryList(groceryList.filter(item => item.Name !== itemName));//update the grocery list by removing the item
       } else {
         console.log("Error removing item");
       }
     });
   };
 
+  /**
+   * Function to add grocery item to inventory and remove from grocery list
+   * @param {String} item - the grocery item
+   * @param {String} category - the category of the grocery item
+   */
   function addToInventory(item, category) {
     // Add the specified grocery item to the inventory and remove it from the grocery list
     if (item === '') {
-      removeItem(category, '');
+      removeItem(category, '');//remove empty items
       return;
     }
 
+    // Add the grocery item to the inventory
     const addToInventory = async (e) => {
       const response = await fetch('/api/add-to-inventory', {
         method: 'POST',
@@ -308,7 +352,7 @@ function Grocery({userdata}) {
         }),
       });
 
-      if (response.status === 200 || response.status === 409) {
+      if (response.status === 200 || response.status === 409) {//if the item is added successfully or already exists, remove it from the grocery list
         console.log("Grocery item added to inventory");
 
         // Remove the grocery item from the list
@@ -341,6 +385,7 @@ function Grocery({userdata}) {
 
   }
 
+  // Render the grocery list
   return (
     <div className="App">
       <header className="App-header">
